52e995d0a430794b68de302df7a742be08882bb3
Aaruni Kaushik <aaruni@edufor.me>
2024-06-21 10:06:31 +0200

    Account for multiple notebooks, and, we don't really need tester.

--- tester.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-nbfilename = sys.argv[1]
-
-#finally run the stuff
-if nbfilename == '':
-    raise AssertionError("cant be empty")
-subprocess.run(f'''jupytext --set-kernel "julia-1.10" --execute {nbfilename}''', shell=True, check=True)
